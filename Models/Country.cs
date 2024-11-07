e1ef7c9d2b25013084fa40d85eaa9a0d18c005cf
GLB_VietTQ <viettran891011@globits.net>
2024-11-07 15:36:22 +0700

    Initial commit

---
+++ Models/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace learnAsp.Models
+{
+    public class Country
+    {
+        public int id { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+        public string? description { get; set; }
+    }
+
+}
